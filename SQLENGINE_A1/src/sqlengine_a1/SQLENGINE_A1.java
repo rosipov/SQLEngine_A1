2fecc451684c496eb5c29f44031ddad605e11b44
rosipov <rosipov@139.62.148.114>
2012-11-10 12:38:19 -0500

    Added Hello World statement

--- SQLENGINE_A1/src/sqlengine_a1/SQLENGINE_A1.java
+++ SQLENGINE_A1/src/sqlengine_a1/SQLENGINE_A1.java
@@ -1,7 +1,6 @@
 package sqlengine_a1;
 public class SQLENGINE_A1 {
-
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Hello team A1"); 
     }
 }
