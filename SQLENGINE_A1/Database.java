316e7ef477680fc0fc11e706e3a9c5861cc7647b
Sepiac <danerussell4@gmail.com>
2012-11-21 20:39:05 -0500

    Update SQLENGINE_A1/Database.java
    Added the system level commands

--- SQLENGINE_A1/Database.java
+++ SQLENGINE_A1/Database.java
@@ -4,27 +4,108 @@ import sqlengine_a1.parser.*;
 
 public class Database {
 	private Map<String, Table> tables;
+	private String name;
 	
-	private Database() {
+	
+	private Database(String name) {
+		this.name = name;
 		tables = new HashMap<String, Table>();
 	}
 	
 	public static Database load(String name) throws SqlException {
-		// ...
-		return new Database();
+		FileInputStream sFile = null;
+		
+		try
+		{
+			sFile = new FileInputStream(name + ".dat");
+		} catch (FileNotFoundException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		ObjectInputStream loader = null;
+		try
+		{
+			loader = new ObjectInputStream(sFile);
+		} catch (IOException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		try
+		{
+			return (Database) loader.readObject();
+		} catch (IOException e)
+		{
+			// TODO Auto-generated catch block
+			System.out.println("1");
+		} catch (ClassNotFoundException e)
+		{
+			// TODO Auto-generated catch block
+			System.out.println("2");
+		}
+		
+		try
+		{
+			loader.close();
+		} catch (IOException e)
+		{
+			// TODO Auto-generated catch block
+			System.out.println("3");
+		}
+		return null;
 	}
 	
 	public static Database create(String name) throws SqlException {
-		// ...
-		return new Database();
+
+		return new Database(name);
 	}
 	
 	public void save() throws SqlException {
-		// ...
+		FileOutputStream sFile = null;
+		try
+		{
+			sFile = new FileOutputStream(name + ".dat");
+		} catch (FileNotFoundException e)
+		{
+			// TODO Auto-generated catch block
+			System.out.println("Could not open file");
+		}
+		
+		ObjectOutputStream saver = null;
+		try
+		{
+			saver = new ObjectOutputStream(sFile);
+		} catch (IOException e)
+		{
+			// TODO Auto-generated catch block
+			System.out.println("Could not initiate output stream.");
+		}
+		
+		try
+		{
+			saver.writeObject(this);
+		} catch (IOException e)
+		{
+			// TODO Auto-generated catch block
+			System.out.println("Could not save the file.");
+		}
+		
+		try
+		{
+			saver.close();
+		} catch (IOException e)
+		{
+			// TODO Auto-generated catch block
+			System.out.println("Could not close the saver.");
+		}
 	}
 	
 	public static void drop(String name) throws SqlException {
-		// ...
+		File file = new File(name+".dat");
+		file.delete();
 	}
 	
 	public void createTable(String name, List<ColumnDefinition> columns) throws SqlException {
